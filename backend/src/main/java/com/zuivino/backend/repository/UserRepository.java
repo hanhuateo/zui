27c46f302953bba111d6716c7924b956f634c5cc
elfique1 <92998661+elfique1@users.noreply.github.com>
2025-06-23 21:17:20 +0800

    chore: separated the controllers, entities and repositories into their respective folders

--- backend/src/main/java/com/zuivino/backend/repository/UserRepository.java
+++ backend/src/main/java/com/zuivino/backend/repository/UserRepository.java
@@ -1,6 +1,8 @@
-package com.zuivino.backend;
+package com.zuivino.backend.repository;
 
 import org.springframework.data.repository.CrudRepository;
+
+import com.zuivino.backend.entity.User;
 
 public interface UserRepository extends CrudRepository<User, Integer>{
     
