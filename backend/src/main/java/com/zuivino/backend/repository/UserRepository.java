e14d80566edca1cf04debf106984130f60a4ec29
Teo Han Hua <hanhuateo@gmail.com>
2025-07-05 16:17:50 +0800

    delete: User.java, UserRepository.java and MainController.java because not in use

--- backend/src/main/java/com/zuivino/backend/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.zuivino.backend.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.zuivino.backend.entity.User;
-
-public interface UserRepository extends CrudRepository<User, Integer>{
-    
-}
