78722a174a12ec0b31aaf08849d7910f9525f127
Teo Han Hua <hanhuateo@gmail.com>
2025-07-05 17:06:31 +0800

    delete: wine.java and winelist.java for refactoring of code, would like to start with restaurant and winelist first

--- backend/src/main/java/com/zuivino/backend/repository/WineListEntryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.zuivino.backend.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.zuivino.backend.entity.WineListEntry;
-
-public interface WineListEntryRepository extends CrudRepository<WineListEntry, Integer>{
-    
-}
--- backend/src/main/java/com/zuivino/backend/repository/WineRepository.java
+++
@@ -1,9 +0,0 @@
-package com.zuivino.backend.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.zuivino.backend.entity.Wine;
-
-public interface WineRepository extends CrudRepository<Wine, Integer>{
-    
-}
